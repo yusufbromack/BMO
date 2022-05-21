d870d0c7949a430d8e6e1ea95f03b200cfa29316
yusufbromack <josephbrownwomack@gmail.com>
2022-05-20 18:00:11 -0700

    Thursday Updates

--- bdomarket/models.py
+++ bdomarket/models.py
@@ -3,41 +3,56 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 
-CATEGORY_MATERIAL = 25
-CATEGORY_CONSUMABLES = 35
-
-CATEGORY_CHOICES = (
-    (CATEGORY_MATERIAL, _('Material')),
-    (CATEGORY_CONSUMABLES, _('Consumables')),
-)
-
 class Item(models.Model):
-    itemId = models.PositiveSmallIntegerField(null=True)
+    itemId = models.PositiveIntegerField(null=True)
     name = models.CharField(max_length=100)
     group = models.CharField(max_length=100)
-    category = models.PositiveSmallIntegerField(
-        choices=CATEGORY_CHOICES,
-        default=CATEGORY_MATERIAL
-    )
 
-    # class SubCategory(models.Model):
-    #     class Category(models.IntegerChoices):
-    #         MAIN_WEAPON = 1
-    #         SUB_WEAPON = 5
-    #         AWAKENING_WEAPON = 10
-    #         ARMOR = 15
-    #         ACCESSORY = 20
-    #         MATERIAL = 25
-    #         ENHANCEMENT_UPGRADE = 30
-    #         CONSUMABLES = 35
-    #         LIFE_TOOLS = 40
-    #         ALCHEMY_STONE = 45
-    #         MAGIC_CRYSTAL = 50
-    #         PEARL_ITEMS = 55
-    #         DYE = 60
-    #         MOUNT = 65
-    #         SHIP = 70
-    #         WAGON = 75
-    #         FURNITURE = 80
-        
-    #     category = models.IntegerField(choices=Category.choices)
+    def __str__(self):
+        return self.name
+
+# class Category(models.IntegerChoices):
+#     MAIN_WEAPON = 1
+#     SUB_WEAPON = 5
+#     AWAKENING_WEAPON = 10
+#     ARMOR = 15
+#     ACCESSORY = 20
+#     MATERIAL = 25
+#     ENHANCEMENT_UPGRADE = 30
+#     CONSUMABLES = 35
+#     LIFE_TOOLS = 40
+#     ALCHEMY_STONE = 45
+#     MAGIC_CRYSTAL = 50
+#     PEARL_ITEMS = 55
+#     DYE = 60
+#     MOUNT = 65
+#     SHIP = 70
+#     WAGON = 75
+#     FURNITURE = 80
+
+#     CHOICES = (
+#     (MAIN_WEAPON, _('Main Weapon')),
+#     (SUB_WEAPON, _('Sub Weapon')),
+#     (AWAKENING_WEAPON, _('Awakening Weapon')),
+#     (ARMOR, _('Armor')),
+#     (ACCESSORY, _('Accessory')),
+#     (MATERIAL, _('Material')),
+#     (ENHANCEMENT_UPGRADE, _('Enhancement/Upgrade')),
+#     (CONSUMABLES, _('Consumables')),
+#     (LIFE_TOOLS, _('Life Tools')),
+#     (ALCHEMY_STONE, _('Alchemy Stone')),
+#     (MAGIC_CRYSTAL, _('Magic Crystal')),
+#     (PEARL_ITEMS, _('Pearl Items')),
+#     (DYE, _('Dye')),
+#     (MOUNT, _('Mount')),
+#     (SHIP, _('Ship')),
+#     (WAGON, _('Wagon')),
+#     (FURNITURE, _('Furniture')),
+#     )
+
+#     category = models.IntegerField(choices=CHOICES)
+
+#     def __str__(self):
+#         return self.category
+
+# class SubCategory(models.IntegerChoices):
--- bdomarket/views.py
+++ bdomarket/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import requests
 from rest_framework import viewsets
+from django.http import HttpResponse, HttpResponseNotFound
+import datetime
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -8,3 +11,23 @@ from .serializers import ItemSerializer
 class ItemViewSet(viewsets.ModelViewSet):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
+
+def date_time(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def item_test_view(request):
+    url = "https://na-trade.console.playblackdesert.com/Trademarket/GetWorldMarketSearchList"
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "BlackDesert"
+    }
+    payload = {
+        "searchResult": "5419,5418"
+    }
+    result = requests.request('POST', url, json=payload, headers=headers)
+    if result.status_code == 200:
+        return HttpResponse("<html><body>%s</body></html>" % result.text)
+        # return HttpResponse('Successful')
+    return HttpResponse('Something Went Wrong')
